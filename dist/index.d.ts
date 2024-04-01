c8f7df1aa2d50ef7de5024edc7680b8cd138fd2e
tom <anytable@gmail.com>
2024-04-01 10:10:54 +0800

    fix global not define

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,11 +3,10 @@ import { Duplex } from 'node:stream';
 import { ProgramNode } from 'rollup';
 import { TransformPluginContext } from 'rollup';
 import { ViteDevServer } from 'vite';
-import { WebSocket as WebSocket_2 } from '@cloudflare/workers-types/experimental/index';
 
 export declare const bind: (path: string, listener: bindFunction) => void;
 
-declare type bindFunction = (server: WebSocket_2, client: WebSocket_2) => void;
+declare type bindFunction = (server: WebSocket, client: WebSocket) => void;
 
 export declare const handle: (req: IncomingMessage_2 | Request, socket: Duplex, head: Buffer) => Promise<Response | undefined>;
 
